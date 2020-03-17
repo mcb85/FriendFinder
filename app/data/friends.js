ca7c3f64cb85a817f9764ae3cdeed3493a1bdc81
Mariana <mariana.diaz@live.com>
2020-03-17 02:39:58 -0400

    create home and survey html pages, server js, friends array

---
+++ app/data/friends.js
@@ -0,0 +1,130 @@
+var friendsArray = [
+    {
+        name: "Ryan Gosling",
+        photo: "https://m.media-amazon.com/images/M/MV5BMTQzMjkwNTQ2OF5BMl5BanBnXkFtZTgwNTQ4MTQ4MTE@._V1_SY1000_CR0,0,790,1000_AL_.jpg",
+        gender: "male",
+        scores: [
+            5,
+            2,
+            4,
+            3,
+            5,
+            3,
+            1,
+            3,
+            2,
+            4
+        ]
+    },
+
+    {
+        name: "Chris Evans",
+        photo: "https://www.caa.com/sites/default/files/styles/headshot_500x500/public/speaker-headshots/EvansC_Headshot_web.jpg?itok=lSM8ZR7M",
+        gender: "male",
+        scores: [
+            3,
+            2,
+            5,
+            4,
+            1,
+            3,
+            3,
+            2,
+            4,
+            5
+        ]
+
+    },
+
+    {
+        name: "Jennifer Aniston",
+        photo: "https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY1200_CR103,0,630,1200_AL_.jpg",
+        gender: "female",
+        scores: [
+            2,
+            3,
+            1,
+            5,
+            2,
+            4,
+            4,
+            2,
+            5,
+            1
+        ]
+    },
+
+    {
+        name: "Scarlett Johansson",
+        photo: "https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_.jpg",
+        gender: "female",
+        scores: [
+            2,
+            3,
+            4,
+            2,
+            1,
+            5,
+            3,
+            4,
+            2,
+            4
+        ]
+        },
+
+        {
+            name: "Ryan Reynolds",
+            photo: "https://m.media-amazon.com/images/M/MV5BOTI3ODk1MTMyNV5BMl5BanBnXkFtZTcwNDEyNTE2Mg@@._V1_UY1200_CR115,0,630,1200_AL_.jpg",
+            gender: "male",
+            scores: [
+                3,
+                4,
+                5,
+                2,
+                1,
+                3,
+                2,
+                1,
+                4,
+                5
+            ]
+        },
+
+        {
+            name: "Blake Lively",
+            photo: "https://m.media-amazon.com/images/M/MV5BMTU2Njk0NTUyNl5BMl5BanBnXkFtZTcwODE0OTgyNw@@._V1_.jpg",
+            gender: "female",
+            scores: [
+                2,
+                4,
+                4,
+                1,
+                1,
+                2,
+                2,
+                1,
+                3,
+                4
+            ]
+        },
+
+        {
+            name: "Brad Pitt",
+            photo: "https://m.media-amazon.com/images/M/MV5BMjA1MjE2MTQ2MV5BMl5BanBnXkFtZTcwMjE5MDY0Nw@@._V1_.jpg",
+            gender: "male",
+            scores: [
+                3,
+                5,
+                4,
+                3,
+                2,
+                1,
+                5,
+                4,
+                3,
+                2
+            ]
+        }
+];
+
+module.exports = friendsArray;

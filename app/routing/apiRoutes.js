19b9de3ebb246048dfbbcafd708c03736c5e2039
Mariana <mariana.diaz@live.com>
2020-03-19 14:37:37 -0400

    added routes

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,23 @@
+var friendsData = require("../data/friends");
+
+
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res){
+       return res.json(friendsData);
+    });
+
+
+    app.post("api/friends", function(req, res) {
+       res.json(friendsData);
+    });
+
+};
+
+var surveyScores = friendsData[i].scores;
+console.log(surveyScores);
+
+
+
+//var match =

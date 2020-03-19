5cdfd6456f79153f4a85cddb7db00389661bb7cc
Mariana <mariana.diaz@live.com>
2020-03-19 17:01:15 -0400

    corrected paths for routes

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,6 +2,7 @@ var friendsData = require("../data/friends");
 
 
 
+
 module.exports = function(app) {
 
     app.get("/api/friends", function(req, res){
@@ -9,14 +10,17 @@ module.exports = function(app) {
     });
 
 
-    app.post("api/friends", function(req, res) {
-       res.json(friendsData);
+    app.post("/api/friends", function(req, res) {
+        res.json(friendsData);
     });
 
 };
 
+for (var i = 0; i < 0; i++) {
 var surveyScores = friendsData[i].scores;
 console.log(surveyScores);
+}
+
 
 
 
--- server.js
+++ server.js
@@ -7,10 +7,9 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-//app.use(express.static("public"));
 
-require("./routing/apiRoutes")(app);
-require("./routing/htmlRoutes")(app);
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);

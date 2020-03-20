41635527f1e95a3660779f0bff4fc747b2b10cf2
Mariana <mariana.diaz@live.com>
2020-03-19 23:50:46 -0400

    add find match logic

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -3,25 +3,39 @@ var friendsData = require("../data/friends");
 
 
 
-module.exports = function(app) {
 
-    app.get("/api/friends", function(req, res){
-       return res.json(friendsData);
+module.exports = function (app) {
+
+    app.get("/api/friends", function (req, res) {
+        return res.json(friendsData);
     });
 
 
-    app.post("/api/friends", function(req, res) {
-        res.json(friendsData);
+    app.post("/api/friends", function (req, res) {
+        var surveyUserData = req.body;
+        var mostCompatibleUser = "";
+        var currentTotalDifference= 0; 
+
+        for (var i = 0; i < friendsData.length; i++) {
+            var friendsScores = friendsData[i].scores;
+            var totalDifference = 0;
+            for (var j = 0; j < friendsScores.length; j++) {
+                var friendScore = friendsScores[j];
+                var userScore = surveyUserData.scores[j];
+                totalDifference = totalDifference + Math.abs(friendScore - userScore);
+            }
+            if (i === 0 || totalDifference < currentTotalDifference ) {
+                mostCompatibleUser=friendsData[i];
+            }
+            console.log(totalDifference);
+            console.log(friendsData[i].name);
+            currentTotalDifference=totalDifference;
+        }
+        console.log("testing compatible user");
+        console.log(mostCompatibleUser);
+        // friendsData.push(req.body);
+        return res.json(mostCompatibleUser);
     });
 
 };
 
-for (var i = 0; i < 0; i++) {
-var surveyScores = friendsData[i].scores;
-console.log(surveyScores);
-}
-
-
-
-
-//var match =
